5d67ba0fb4d8acab8f752337ad63bbedf4b15726
Koo-29 <koogang@graduate.utm.my>
2026-04-17 16:52:40 +0800

    add admin account

--- lib/models/financial/bnpl_plan.dart
+++ lib/models/financial/bnpl_plan.dart
@@ -40,35 +40,35 @@ class BnplPlan {
   }
 
   Map<String, dynamic> toMap() => {
-        'itemName': itemName,
-        'totalAmount': totalAmount,
-        'installments': installments,
-        'paidInstallments': paidInstallments,
-        'monthlyAmount': monthlyAmount,
-        'nextDueDate': nextDueDate.millisecondsSinceEpoch,
-        'nextDueDay': nextDueDay,
-        'lateFees': lateFees,
-        'status': status.name,
-      };
+    'itemName': itemName,
+    'totalAmount': totalAmount,
+    'installments': installments,
+    'paidInstallments': paidInstallments,
+    'monthlyAmount': monthlyAmount,
+    'nextDueDate': nextDueDate.millisecondsSinceEpoch,
+    'nextDueDay': nextDueDay,
+    'lateFees': lateFees,
+    'status': status.name,
+  };
 
   factory BnplPlan.fromMap(String id, Map<String, dynamic> map) => BnplPlan(
-        id: id,
-        itemName: map['itemName'] as String,
-        totalAmount: (map['totalAmount'] as num).toDouble(),
-        installments: map['installments'] as int,
-        paidInstallments: map['paidInstallments'] as int? ?? 0,
-        monthlyAmount: (map['monthlyAmount'] as num).toDouble(),
-        nextDueDate: map['nextDueDate'] is int
-            ? DateTime.fromMillisecondsSinceEpoch(map['nextDueDate'] as int)
-            : map['nextDueDate'] is DateTime
-                ? map['nextDueDate'] as DateTime
-                : map['nextDueDate'] is Timestamp
-                    ? (map['nextDueDate'] as Timestamp).toDate()
-                    : DateTime.now(),
-        nextDueDay: map['nextDueDay'] as int?,
-        lateFees: (map['lateFees'] as num?)?.toDouble() ?? 0,
-        status: BnplStatus.values.byName(map['status'] as String),
-      );
+    id: id,
+    itemName: map['itemName'] as String,
+    totalAmount: (map['totalAmount'] as num).toDouble(),
+    installments: map['installments'] as int,
+    paidInstallments: map['paidInstallments'] as int? ?? 0,
+    monthlyAmount: (map['monthlyAmount'] as num).toDouble(),
+    nextDueDate: map['nextDueDate'] is int
+        ? DateTime.fromMillisecondsSinceEpoch(map['nextDueDate'] as int)
+        : map['nextDueDate'] is DateTime
+        ? map['nextDueDate'] as DateTime
+        : map['nextDueDate'] is Timestamp
+        ? (map['nextDueDate'] as Timestamp).toDate()
+        : DateTime.now(),
+    nextDueDay: map['nextDueDay'] as int?,
+    lateFees: (map['lateFees'] as num?)?.toDouble() ?? 0,
+    status: BnplStatus.values.byName(map['status'] as String),
+  );
 }
 
 enum BnplStatus { active, paid, defaulted }
